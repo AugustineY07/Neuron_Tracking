58340c424388c59c941ca278ca44c6619b1f7651
Augustine Yuan <43792697+AugustineY07@users.noreply.github.com>
2023-08-03 18:30:41 -0400

    Delete Pipeline directory

--- Pipeline/drift_correction/correct_drift.m
+++
@@ -1,8 +0,0 @@
-function correct_drift(z_drift,locdir2,id,ish)
-        centXZY = readNPY([locdir2,'\centXZY.npy']);
-        centXZY_corrected = centXZY;
-        centXZY_corrected(:,2) = centXZY(:,2) - z_drift;
-        writeNPY(centXZY_corrected,[locdir2,'\centXZY_corrected.npy'])
-end
-
-
