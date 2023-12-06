89edf3274f6b92a679a0e4a2faa2bc89dce2673d
Augustine Yuan <43792697+AugustineY07@users.noreply.github.com>
2023-12-06 17:52:51 -0500

    Add files via upload

--- Pipeline/NT_main.m
+++ Pipeline/NT_main.m
@@ -1,8 +1,7 @@
 % This is the main function of the algorithm
 % Input: kilosort cluster label, channel map, mean waveforms, 
 % Output: Unit match assignment
-% For more comparisons, users need to write their own loops; see
-% Example_run.m
+% For more comparisons, users need to write their own loops
 function NT_main(input,chan_pos,mwf1,mwf2)
 
 % Estimate location 
@@ -13,13 +12,18 @@ wf_metrics2 = wave_metrics(mwf2, chan_pos, input);
 output.threshold = input.threshold;
 output.z_mode = 0;
 output = create_EMD_input(input, output, wf_metrics1, wf_metrics2, mwf1, mwf2, 'pre'); 
+fprintf('EMD_pre input created! \n')
 output = EMD_unit_match(input,output,'pre');
+fprintf('Pre-correction match found! \n')
 [output.diffZ,edges] = z_estimate(input);
 output.z_mode = kernelModeEstimate(output.diffZ);
+fprintf('Drift detected! \n')
 
 % EMD unit matching
 output = create_EMD_input(input, output, wf_metrics1, wf_metrics2, mwf1, mwf2, 'post'); 
+fprintf('EMD_post input created! \n')
 output = EMD_unit_match(input,output,'post');
+fprintf('Matches found! \n')
 
 % thresholding
 output.results_wth = output.all_results_post(output.all_results_post(:,7) <= input.threshold,:); 
